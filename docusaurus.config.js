dfbede3c1c03305862ee4239535e71d5c454accc
Sam Foo <sam.foo@iohk.io>
2023-05-17 18:24:48 -0700

    Override infima variables and remove unnecessary documents

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,8 +63,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       navbar: {
         title: '',
         logo: {
@@ -106,63 +109,6 @@ const config = {
             position: 'right',
           },
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Getting Started',
-                to: '/docs/introduction',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Github',
-                href: 'https://github.com/input-output-hk/marlowe',
-              },
-              {
-                label: 'Stack Exchange',
-                href: 'https://cardano.stackexchange.com/',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/inputoutput',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/InputOutputHK',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Privacy Policy',
-                href: 'https://static.iohk.io/gdpr/IOHK-Data-Protection-GDPR-Policy.pdf',
-              },
-              {
-                label: 'Terms and Conditions',
-                href: 'https://plutus-static.s3.eu-central-1.amazonaws.com/IOHK+Website+Terms+%26+Conditions+(Final).pdf',
-              },
-              {
-                label: 'iohk.io',
-                href: 'https://iohk.io',
-              },
-              {
-                label: 'cardano.org',
-                href: 'https://cardano.org',
-              },
-            ],
-          },
-        ],
-        // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
